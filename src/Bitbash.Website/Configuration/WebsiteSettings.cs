7c2667b7c22a1b9cbe98c0f2aaca89b90b217e93
Sander Molenkamp <sander@sandermolenkamp.com>
2023-10-29 10:30:57 +0100

    New Blazor WASM version

---
+++ src/Bitbash.Website/Configuration/WebsiteSettings.cs
@@ -0,0 +1,10 @@
+namespace Bitbash.Configuration;
+
+public class WebsiteSettings
+{
+    public string CurrentEdition { get; set; } = string.Empty;
+
+    public bool SpeakersAnnounced { get; set; } = false;
+
+    public bool TicketsOnSale { get; set; } = false;
+}
