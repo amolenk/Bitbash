ec15e9450daa4dfc80e96baf56fa8229303e5b7f
Sander Molenkamp <sander@sandermolenkamp.com>
2023-10-29 11:21:51 +0100

    Temp disable JS

--- src/Bitbash.Website/wwwroot/js/main.js
+++ src/Bitbash.Website/wwwroot/js/main.js
@@ -4,35 +4,35 @@
     /**
      * Easy selector helper function
      */
-    // const select = (el, all = false) => {
-    //     el = el.trim()
-    //     if (all) {
-    //         return [...document.querySelectorAll(el)]
-    //     } else {
-    //         return document.querySelector(el)
-    //     }
-    // }
+    const select = (el, all = false) => {
+        el = el.trim()
+        if (all) {
+            return [...document.querySelectorAll(el)]
+        } else {
+            return document.querySelector(el)
+        }
+    }
 
     /**
      * Easy event listener function
      */
-    // const on = (type, el, listener, all = false) => {
-    //     let selectEl = select(el, all)
-    //     if (selectEl) {
-    //         if (all) {
-    //             selectEl.forEach(e => e.addEventListener(type, listener))
-    //         } else {
-    //             selectEl.addEventListener(type, listener)
-    //         }
-    //     }
-    // }
+    const on = (type, el, listener, all = false) => {
+        let selectEl = select(el, all)
+        if (selectEl) {
+            if (all) {
+                selectEl.forEach(e => e.addEventListener(type, listener))
+            } else {
+                selectEl.addEventListener(type, listener)
+            }
+        }
+    }
 
     /**
      * Easy on scroll event listener 
      */
-    // const onscroll = (el, listener) => {
-    //     el.addEventListener('scroll', listener)
-    // }
+    const onscroll = (el, listener) => {
+        el.addEventListener('scroll', listener)
+    }
 
     /**
      * Navbar links active state on scroll
@@ -153,22 +153,22 @@ function InitializeHeaderScrollEffect() {
  */
 function InitializeMobileNavigation() {
     
-    // Mobile nav toggle
-    on('click', '.mobile-nav-toggle', function (e) {
-        select('#navbar').classList.toggle('navbar-mobile')
-        this.classList.toggle('bi-list')
-        this.classList.toggle('bi-x')
-    })
+    // // Mobile nav toggle
+    // on('click', '.mobile-nav-toggle', function (e) {
+    //     select('#navbar').classList.toggle('navbar-mobile')
+    //     this.classList.toggle('bi-list')
+    //     this.classList.toggle('bi-x')
+    // })
 
-    /**
-     * Mobile nav dropdowns activate
-     */
-    on('click', '.navbar .dropdown > a', function (e) {
-        if (select('#navbar').classList.contains('navbar-mobile')) {
-            e.preventDefault()
-            this.nextElementSibling.classList.toggle('dropdown-active')
-        }
-    }, true)
+    // /**
+    //  * Mobile nav dropdowns activate
+    //  */
+    // on('click', '.navbar .dropdown > a', function (e) {
+    //     if (select('#navbar').classList.contains('navbar-mobile')) {
+    //         e.preventDefault()
+    //         this.nextElementSibling.classList.toggle('dropdown-active')
+    //     }
+    // }, true)
 }
 
 
