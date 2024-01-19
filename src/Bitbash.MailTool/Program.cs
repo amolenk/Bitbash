ec763ce6e189b5ce85c633f1a1745a551b0effdc
Sander Molenkamp <sander@sandermolenkamp.com>
2024-01-19 09:30:47 +0100

    Rebase

---
+++ src/Bitbash.MailTool/Program.cs
@@ -0,0 +1,14 @@
+﻿using Bitbash.MailTool;
+
+switch (args)
+{
+    case ["mailmerge", _, _]:
+        new MailMergeInputBuilder(args[1]).Build(args[2]);
+        break;
+    case ["results", _]:
+        new ResultsBuilder(args[1]).Build();
+        break;
+    default:
+        Console.WriteLine("Usage: Bitbash.MailTool <command> <workingFolder>");
+        break;
+}
