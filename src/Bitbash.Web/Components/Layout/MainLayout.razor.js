52011e486ce5fc721e8d9118ace90600d5bc6019
Sander Molenkamp <sander@sandermolenkamp.com>
2024-05-01 13:27:41 +0200

    Fix AOS init

--- src/Bitbash.Web/Components/Layout/MainLayout.razor.js
+++ src/Bitbash.Web/Components/Layout/MainLayout.razor.js
@@ -1,7 +1,15 @@
 export function onLoad() {
 
+    AOS.init({
+        duration: 1000,
+        easing: 'ease-in-out',
+        once: true,
+        mirror: false
+    })
+
     // Animate on scroll
     window.addEventListener('load', () => {
+
         AOS.init({
             duration: 1000,
             easing: 'ease-in-out',
