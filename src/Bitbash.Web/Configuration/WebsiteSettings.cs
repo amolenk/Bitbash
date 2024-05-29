4c66597beae9c1ebc5e5ff272d1f0ed9a066d636
Sander Molenkamp <sander@sandermolenkamp.com>
2024-05-29 16:32:12 +0200

    HubSpot integration

--- src/Bitbash.Web/Configuration/WebsiteSettings.cs
+++ src/Bitbash.Web/Configuration/WebsiteSettings.cs
@@ -9,6 +9,11 @@ public class WebsiteSettings
     public DateOnly ConferenceDate { get; init; }
     public bool AgendaAnnounced { get; init; } = false;
     public bool SpeakersAnnounced { get; init; } = false;
+    public bool ConferenceTicketSaleOpened { get; init; } = true;
+    public bool WorkshopTicketSaleOpened { get; init; } = false;
     public string? SessionizeCfpLink { get; init; }
     public string? EventbriteLink { get; init; }
+    public string? HubSpotRegion { get; init; }
+    public string? HubSpotPortalId { get; init; }
+    public string? HubSpotConferenceTicketFormId { get; init; }
 }
