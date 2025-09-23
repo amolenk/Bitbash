11933e607c85026e2d9d07ab2379d8674001248f
Sander Molenkamp <sander@sandermolenkamp.com>
2025-09-23 17:54:57 +0200

    Fix error message

--- src-react/app/[edition]/speaker/[speakerId]/page.tsx
+++ src-react/app/[edition]/speaker/[speakerId]/page.tsx
@@ -40,6 +40,6 @@ export default async function SpeakerDetailPage({
             </MainLayout>
         );
     } catch (error) {
-        return <div>Failed to load session.</div>;
+        return <div>Failed to load speaker.</div>;
     }
 }
