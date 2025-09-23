e587f0fcad79d967510e02044611b7b9c564ac82
Sander Molenkamp <sander@sandermolenkamp.com>
2025-09-23 07:57:07 +0200

    Disable CFP for now

--- src-react/src/components/sections/CallForPapers.tsx
+++ src-react/src/components/sections/CallForPapers.tsx
@@ -2,21 +2,24 @@ import React from "react";
 import { websiteSettings } from "../../config/website-settings";
 
 export default function CallForPapers() {
-  const now = new Date();
-  const open = new Date(websiteSettings.cfpOpenTime);
-  const close = new Date(websiteSettings.cfpCloseTime);
-  const isOpen = websiteSettings.sessionizeCfpLink && now >= open && now <= close;
-  if (!isOpen) return null;
-  return (
-    <div className="row text-center my-4">
-      <h2>Interested in speaking at Bitbash?</h2>
-      <p>
-        Our{' '}
-        <a href={websiteSettings.sessionizeCfpLink} target="_blank" rel="noopener noreferrer">
-          call for papers
-        </a>{' '}
-        is now open!
-      </p>
-    </div>
-  );
+
+    return null;
+
+//   const now = new Date();
+//   const open = new Date(websiteSettings.cfpOpenTime);
+//   const close = new Date(websiteSettings.cfpCloseTime);
+//   const isOpen = websiteSettings.sessionizeCfpLink && now >= open && now <= close;
+//   if (!isOpen) return null;
+//   return (
+//     <div className="row text-center my-4">
+//       <h2>Interested in speaking at Bitbash?</h2>
+//       <p>
+//         Our{' '}
+//         <a href={websiteSettings.sessionizeCfpLink} target="_blank" rel="noopener noreferrer">
+//           call for papers
+//         </a>{' '}
+//         is now open!
+//       </p>
+//     </div>
+//   );
 }
