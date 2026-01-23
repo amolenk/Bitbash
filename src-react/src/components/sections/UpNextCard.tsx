b15d22a170539dcc3f017ef58e048e8f7c35cbca
Sander Molenkamp <sander@sandermolenkamp.com>
2026-01-23 18:39:57 +0100

    Enlarge up-next

--- src-react/src/components/sections/UpNextCard.tsx
+++ src-react/src/components/sections/UpNextCard.tsx
@@ -9,11 +9,11 @@ export default function UpNextCard({session}: { session: SessionWithSpeakers }) 
         <div
             className={`${styles.upNextCard} card h-100 w-100`}
         >
-            <div className="card-header text-muted pb-0"><h2>{session.Room}</h2></div>
+            <div className="card-header text-muted pb-0"><h1>{session.Room}</h1></div>
             <div className={`${styles.cardBody} card-body`}>
-                <h3 className={`${styles.cardTitle} mb-4`}>{session.Title}</h3>
+                <h1 className={`${styles.cardTitle} mb-4`}>{session.Title}</h1>
                 {session.Speakers && session.Speakers.map(speaker => (
-                    <p key={speaker.Id}>
+                    <h1 key={speaker.Id}>
                         <img
                             loading="lazy"
                             src={speaker.ProfilePictureUrl}
@@ -21,17 +21,17 @@ export default function UpNextCard({session}: { session: SessionWithSpeakers }) 
                             alt={`Photo of ${speaker.FullName}`}
                         />
                         {speaker.FullName}
-                    </p>
+                    </h1>
                 ))}
             </div>
-            <div className="card-footer bg-transparent border-0">
-                {session.SessionFormat && (
-                    <span className="badge bg-info rounded-pill m-1">{session.SessionFormat}</span>
-                )}
-                {session.Level && (
-                    <span className="badge bg-primary rounded-pill m-1">{session.Level}</span>
-                )}
-            </div>
+            {/*<div className="card-footer bg-transparent border-0">*/}
+            {/*    {session.SessionFormat && (*/}
+            {/*        <span className="badge bg-info rounded-pill m-1">{session.SessionFormat}</span>*/}
+            {/*    )}*/}
+            {/*    {session.Level && (*/}
+            {/*        <span className="badge bg-primary rounded-pill m-1">{session.Level}</span>*/}
+            {/*    )}*/}
+            {/*</div>*/}
         </div>
     );
 }
