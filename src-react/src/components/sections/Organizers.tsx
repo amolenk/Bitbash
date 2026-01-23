94313d721a61326cbf44997e660178c7e499de24
Sander Molenkamp <sander@sandermolenkamp.com>
2026-01-23 21:34:34 +0100

    Update organizers

--- src-react/src/components/sections/Organizers.tsx
+++ src-react/src/components/sections/Organizers.tsx
@@ -13,14 +13,15 @@ export default function Organizers() {
     { name: "Tom van den Berg", image: "/img/organizers/tom.jpg" },
     { name: "Edwin van Wijk", image: "/img/organizers/edwin.jpg" },
     { name: "Sigrid van Tuil-Oosting", image: "/img/organizers/sigrid.jpg" },
+    { name: "Yael Bakker", image: "/img/organizers/yael.jpg" },
   ];
 
   return (
     <Section id="organizers" headerText="Organizers" fadeUp={true} extraClass="justify-content-center text-center my-4">
       <div className="row">
-        {organizers.slice(0, 2).map((organizer, index) => (
+        {organizers.slice(0, 3).map((organizer, index) => (
           <div key={index} className={`col ${styles.organizerItem} text-center`}>
-            <p className="lead text-light">
+            <p className="text-light">
               <span className={styles.organizer}>
                 <img src={organizer.image} alt={organizer.name} />
               </span>
@@ -30,9 +31,9 @@ export default function Organizers() {
         ))}
       </div>
       <div className="row">
-        {organizers.slice(2).map((organizer, index) => (
+        {organizers.slice(3).map((organizer, index) => (
           <div key={index} className={`col ${styles.organizerItem} text-center`}>
-            <p className="lead text-light">
+            <p className="text-light">
               <span className={styles.organizer}>
                 <img src={organizer.image} alt={organizer.name} />
               </span>
